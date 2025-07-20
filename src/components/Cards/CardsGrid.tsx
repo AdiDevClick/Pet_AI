6433897f2324bcdc5f42639eadb0340899c93bb9
Adrien <adrienquijo@gmail.com>
2025-07-20 23:04:44 +0200

    La grille contient maintenant des éléments génériques pour fluidifier la visibilité -

--- src/components/Cards/CardsGrid.tsx
+++ src/components/Cards/CardsGrid.tsx
@@ -1,16 +1,24 @@
-import { Card } from '@/components/Cards/Card.tsx';
-import '@css/images-grid.scss';
+import { TrainingCard } from '@/components/Cards/TrainingCard';
+import { GenericGrid } from '@/components/Grid/GenericGrid.tsx';
+import '@css/card.scss';
+import '@css/generic-grid.scss';
 
 export function CardsGrid({
     images,
+    animalName,
 }: {
     images: Array<{ id: string; url: string; description: string }>;
+    animalName: string;
 }) {
     return (
-        <div className="images-grid_container" id="images-container">
+        <GenericGrid>
             {images.map((image) => (
-                <Card key={image.id} image={image} />
+                <TrainingCard
+                    key={image.id}
+                    image={image}
+                    animalName={animalName}
+                />
             ))}
-        </div>
+        </GenericGrid>
     );
 }
