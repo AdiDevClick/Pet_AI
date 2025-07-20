cf3b2af046d8392627659c386028660eaa0eaf3f
Adrien <adrienquijo@gmail.com>
2025-07-20 23:57:56 +0200

    Création d'un composant permettrant d'insérer une description depuis le layout générique

---
+++ src/components/Texts/GenericDescription.tsx
@@ -0,0 +1,19 @@
+import { HTMLProps, ReactNode } from 'react';
+
+export function GenericDescription<T extends HTMLProps<HTMLParagraphElement>>({
+    children,
+    ...props
+}: {
+    children: ReactNode;
+} & T) {
+    return (
+        <p
+            {...props}
+            className={`generic-layout__description ${
+                props.className ? props.className : ''
+            }`}
+        >
+            {children}
+        </p>
+    );
+}
