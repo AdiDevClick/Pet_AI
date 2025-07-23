a02fd542e93b3d40e1c3aee18927016a6e565d41
Adrien <adrienquijo@gmail.com>
2025-07-23 16:28:28 +0200

    Ajout d'une fonction nécessaire à la validation de masse - Ajout d'une fonction pour sauvegarder l'array d'image dans le localStorage. Cela permet de récupérer plus tard l'array et de réappliquer les tensor dessus - Ajout d'une fonction permettant de load l'array depuis le localStorage -
    Ces fonctions utilisent toutes l'objet global window.animalIdentifier -
    Elles devraient à terme être remplacées par un hook global

--- src/components/Controls/controlsFunctions.ts
+++ src/components/Controls/controlsFunctions.ts
@@ -68,13 +68,36 @@ export async function saveModel({ e, data }) {
     try {
         await window.animalIdentifier.saveModel();
         alert('💾 Modèle sauvegardé avec succès!');
-        // if (data) {
-        //     window.localStorage.setItem('chat-classifier', JSON.stringify(data));
-        //     alert('💾 Modèle sauvegardé avec succès!');
-        // }
     } catch (error) {
         console.error('Erreur de sauvegarde:', error);
         alert('❌ Erreur lors de la sauvegarde du modèle');
+    }
+}
+
+export async function saveData({ e }) {
+    e.preventDefault();
+
+    try {
+        await window.animalIdentifier.saveTrainingPairs();
+        alert('💾 Données sauvegardées avec succès!');
+    } catch (error) {
+        console.error('Erreur de sauvegarde:', error);
+        alert('❌ Erreur lors de la sauvegarde des données');
+    }
+}
+
+export async function loadDefaultDataArray({ e }) {
+    e.preventDefault();
+
+    try {
+        await window.animalIdentifier.loadDefaultDataArray();
+        alert(
+            '📂 Données de comparaison par défaut chargées avec succès!\nUn nouvel enregistrement de données sera créé.'
+        );
+    } catch (error) {
+        alert(
+            '❌ Erreur lors du chargement des données de comparaison par défaut'
+        );
     }
 }
 
@@ -156,6 +179,66 @@ export async function predictAllImages({
             }
         }
     }
+
+    // updateStats();
+}
+export async function validateAllImages({ e }) {
+    e.preventDefault();
+    let count = 0;
+    const comparisons = document.querySelectorAll('.card__image-choice');
+    await Promise.all(
+        Array.from(comparisons).map(async (comparison) => {
+            const images = comparison.querySelectorAll('img');
+            if (images.length === 2) {
+                const image1 = images[0];
+                const image2 = images[1];
+                if (image1.complete && image2.complete) {
+                    await window.animalIdentifier.addTrainingPair(
+                        [image1, image2],
+                        true
+                    );
+                    count++;
+                    // const predictionElement = document.getElementById(
+                    //     `prediction-${image1.id}`
+                    // );
+                    // predictionElement.innerHTML = `
+                    //     <strong>🔮 Prédiction IA:</strong><br>
+                    //     ✅ Images validées<br>
+                    //     <small>Confiance: 100%</small>
+                    // `;
+                    // predictionElement.style.display = 'block';
+                }
+            }
+        })
+    );
+    alert(`✅ Toutes les images(${count}) ont été validées!`);
+    // for (let img of images) {
+    //     if (img.complete) {
+    //         const prediction = await window.imageClassifier.predict(img);
+    //         if (prediction) {
+    //             const imageId = img
+    //                 .closest('.image-card')
+    //                 .id.replace('card-', '');
+    //             const predictionElement = document.getElementById(
+    //                 `prediction-${imageId}`
+    //             );
+
+    //             predictionElement.innerHTML = `
+    //                         <strong>🔮 Prédiction IA:</strong><br>
+    //                         ${
+    //                             prediction.prediction === 'correct'
+    //                                 ? '✅ Chat détecté'
+    //                                 : '❌ Pas un chat'
+    //                         }<br>
+    //                         <small>Confiance: ${(
+    //                             prediction.confidence * 100
+    //                         ).toFixed(1)}%</small>
+    //                     `;
+    //             predictionElement.style.display = 'block';
+    //             setPredictionsCount((prevCount) => prevCount + 1);
+    //         }
+    //     }
+    // }
 
     // updateStats();
 }
