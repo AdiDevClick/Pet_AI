9715df91df205ec19f9f36b683997c6d768de8c4
Adrien <adrienquijo@gmail.com>
2025-07-23 21:50:01 +0200

    Création d'un composant <Icon/> qui peut renvoyer un svg complet - A l'heure actuelle, Vite renvoit le path avec une extension ?import. Ce qui empêche la création de l'icône...

---
+++ src/components/Icons/Icon.tsx
@@ -0,0 +1,13 @@
+import { useDynamicSVGImport } from '@/hooks/useDynamicSVGImport';
+
+export function Icon({ icon, ...props }) {
+    const { SvgIcon, error } = useDynamicSVGImport(icon);
+    if (error) {
+        console.log(error);
+        return <div>Can't load the icon {icon}</div>;
+    }
+    if (SvgIcon) {
+        return <SvgIcon {...props} />;
+    }
+    return <div>Loading icon...</div>;
+}
