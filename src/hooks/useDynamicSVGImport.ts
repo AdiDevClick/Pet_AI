625b18a5963e19d51de8748e32e44cf0542b5e4d
Adrien <adrienquijo@gmail.com>
2025-07-24 12:31:42 +0200

    Fix du hook - Il importe maintenant en tant que glob les svg du projet - Il retourne toujours <SvgIcon /> mais il sera maintenant rréellement vu comme un composant React correct -

--- src/hooks/useDynamicSVGImport.ts
+++ src/hooks/useDynamicSVGImport.ts
@@ -1,22 +1,29 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const icons = import.meta.glob(`../assets/icons/*.svg`, {
+    import: 'default',
+});
 
 export function useDynamicSVGImport(icon: { path: string }, options = {}) {
-    const importedIconRef = useRef(null);
+    const [SvgIcon, setSvgIcon] = useState(null);
     const [error, setError] = useState('');
 
     useEffect(() => {
         const importIcon = async () => {
             try {
-                importedIconRef.current = (
-                    await import(`../assets/icons/${icon.path}.svg`)
-                ).ReactComponent;
-                console.log(importedIconRef.current);
+                const key = `../assets/icons/${icon.path}.svg`;
+                if (!icons[key]) {
+                    throw new Error(`Icon not found: ${key}`);
+                }
+                const path = key + '?react';
+                const module = await import(path);
+                setSvgIcon(() => module.default);
             } catch (error) {
                 setError(error);
             }
         };
         importIcon();
-    }, [icon.path]);
+    }, [icon]);
 
-    return { SvgIcon: importedIconRef.current, error };
+    return { SvgIcon, error };
 }
