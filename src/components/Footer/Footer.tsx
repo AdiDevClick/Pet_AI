2e2f88cc85760c05c4ae2250a5fbb2c0bba882f6
Adrien <adrienquijo@gmail.com>
2025-07-25 11:04:15 +0200

    Importation du CSS - Mise à jour du contenu du footer -

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,14 @@
 import { SocialContainer } from '@/components/Social/SocialContainer.tsx';
+import '@css/footer.scss';
 
 export function Footer() {
     return (
         <footer className="footer">
-            <SocialContainer className="footer__social" />
-            <p>
-                <strong>Note:</strong> Ce projet est en cours de développement
-                et peut contenir des bugs. <br />
-                N'hésitez pas à contribuer ou à signaler des problèmes sur le
-                dépôt GitHub.
-            </p>
-            <p>© 2025 Pet AI. All rights reserved.</p>
-            <p>Made with ❤️ by the Pet AI Team</p>
+            <p className="footer__note">Made with ❤️ by the Pet AI Team</p>
+            <div className="footer__rights">
+                <SocialContainer className="footer__social" />
+                <p>© 2025 Pet AI. All rights reserved.</p>
+            </div>
         </footer>
     );
 }
