1980266a670a839fe47517525a91ea0fa639dd0a
Adrien <adrienquijo@gmail.com>
2025-07-25 12:25:26 +0200

    Le bouton peut maintenant s'activer quand une certaine distance de scroll est effectuée par l'utilisateur - Il est possible de définir une distance à l'aide de la variable UIScrollDistance du fichier UIScroll.config.ts -

--- src/components/Buttons/ScrollTop.tsx
+++ src/components/Buttons/ScrollTop.tsx
@@ -1,21 +1,46 @@
 import { Button } from '@/components/Buttons/Button.tsx';
+import { UIScrollDistance } from '@/configs/UIScroll.config.ts';
 import '@css/button.scss';
+import { useEffect, useState } from 'react';
 
+/**
+ * A simple button that scrolls the page to the top when clicked.
+ *
+ * @description You can define the distance to scroll before the button appears
+ * using the `UIScrollDistance` constant from the `UIScroll.config.ts` file
+ */
 export function ScrollTop() {
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-        });
-    };
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > UIScrollDistance);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <Button
-            className="scroll-top"
-            onClick={scrollToTop}
-            aria-label="Scroll to top"
-        >
-            ⬆️
-        </Button>
+        (isScrolled && (
+            <Button
+                className="scroll-top"
+                onClick={scrollToTop}
+                aria-label="Scroll to top"
+            >
+                ⬆️
+            </Button>
+        )) ||
+        null
     );
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
 }
---
+++ src/configs/UIScroll.config.ts
@@ -0,0 +1,7 @@
+/**
+ * UIScroll.config.ts
+ * @description Configuration for UIScroll component.
+ */
+
+/** Distance from the top before the reset scroll button appears */
+export const UIScrollDistance = 300;
