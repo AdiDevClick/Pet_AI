780467fe4e83459aba983f88021d9510ab0987fe
Adrien <adrienquijo@gmail.com>
2025-07-25 11:03:52 +0200

    mise à jour de l'importation du CSS -

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode, Suspense, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { App } from '@/App.tsx';
+import '@css/index.scss';
+
 import {
     createBrowserRouter,
     Navigate,
