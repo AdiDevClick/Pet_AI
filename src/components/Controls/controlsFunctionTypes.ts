96de065e8e26be8021e545ad749dee2bd6561572
Adrien <adrienquijo@gmail.com>
2025-07-27 00:28:57 +0200

    Création d'un fichier de types pour les composants de <Controls/> et ses fonctions associées

--- src/components/Controls/controlsFunctionTypes.ts
+++
@@ -1,6 +0,0 @@
-import type { MouseEvent } from 'react';
-
-export type LoadModelTypes = {
-    e: MouseEvent<HTMLButtonElement>;
-    setIsSuccess: (value: boolean) => void;
-};
---
+++ src/components/Controls/controlsTypes.ts
@@ -0,0 +1,17 @@
+import type { contextTypes } from '@/mainTypes.ts';
+import type { Dispatch, MouseEvent, SetStateAction } from 'react';
+
+export type LoadModelTypes = {
+    e: MouseEvent<HTMLButtonElement>;
+    setIsSuccess: ControlsFunctionPropsTypes['setIsSuccess'];
+};
+
+export interface ControlsFunctionPropsTypes extends contextTypes {
+    setIsSuccess: Dispatch<SetStateAction<ControlsStateTypes>>;
+}
+
+export type ControlsStateTypes = {
+    status: boolean;
+    id: string | null;
+    error: string | null;
+};
