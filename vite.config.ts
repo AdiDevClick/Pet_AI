bcb13faf712c9e4bade93f58917bce1515e9bf53
Adrien <adrienquijo@gmail.com>
2025-07-26 10:43:39 +0200

    Configuration de vite pour tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), tailwindcss()],
     base: '/',
     css: {
         devSourcemap: true,
