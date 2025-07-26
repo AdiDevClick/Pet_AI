4009c32bbe1cee4f64e0a3dc6db89ac48a61235d
Adrien <adrienquijo@gmail.com>
2025-07-27 00:42:18 +0200

    Ajout de la doc ainsi que le typage du children

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
-export function App({ children }) {
+import type { ReactNode } from 'react';
+
+/**
+ * @description This component serves as the main layout for the application, wrapping all child components.
+ *
+ * @param children - The children components to be rendered inside the main container.
+ * @returns
+ */
+export function App({ children }: { children: ReactNode }) {
     return <main className="main-container">{children}</main>;
 }
