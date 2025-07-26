50d5a42b0a69303c3db209cbd7c7415c77866a0c
Adrien <adrienquijo@gmail.com>
2025-07-27 00:27:47 +0200

    Les boutons du composant <Controls/> peuvent maintenant être configurés depuis le fichier controls.config.ts - Ajout des types associés au composant - Le composant assigne maintenant les functionProps qui seront passées à la config -

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,99 +1,28 @@
 import { AlertDialogButton } from '@/components/Alerts/AlertDialogButton';
 import { Button } from '@/components/Buttons/Button';
-import {
-    loadDefaultDataArray,
-    loadModel,
-    loadNewImages,
-    predictAllImages,
-    resetSystem,
-    saveData,
-    saveModel,
-    trainModel,
-    validateAllImages,
-} from '@/components/Controls/controlsFunctions.ts';
+import type { ControlsStateTypes } from '@/components/Controls/controlsTypes.ts';
+import { clickableButtons, functionProps } from '@/configs/controls.config.ts';
+import type { contextTypes } from '@/mainTypes.ts';
 import '@css/controls.scss';
 import { useState } from 'react';
 import { useOutletContext } from 'react-router-dom';
 
-let functionProps = {};
-const clickableButtons = [
-    {
-        id: 'controls-button-0',
-        label: '🔄 Nouvelles Images',
-        className: 'primary',
-        functions: { onClick: (e) => loadNewImages({ e, ...functionProps }) },
-    },
-    {
-        id: 'controls-button-1',
-        label: '🗑️ Réinitialiser',
-        className: 'danger',
-        functions: { onClick: (e) => resetSystem({ e, ...functionProps }) },
-    },
-    {
-        id: 'controls-button-2',
-        label: '✅ Valider toutes les Images',
-        className: 'success',
-        functions: {
-            onClick: (e) => validateAllImages({ e, ...functionProps }),
-        },
-    },
-    {
-        id: 'controls-button-3',
-        label: '🔮 Prédire Tout',
-        className: 'success',
-        functions: {
-            onClick: (e) => predictAllImages({ e, ...functionProps }),
-        },
-    },
-    {
-        id: 'controls-button-4',
-        label: '🔧 Entraîner le Modèle',
-        className: 'success',
-        functions: {
-            onClick: (e) => trainModel({ e, ...functionProps }),
-        },
-    },
-    {
-        id: 'controls-button-5',
-        label: '💾 Sauvegarder le modèle',
-        className: 'primary',
-        functions: { onClick: (e) => saveModel({ e, ...functionProps }) },
-    },
-    {
-        id: 'controls-button-6',
-        label: '📂 Charger le modèle',
-        className: 'primary',
-        functions: { onClick: (e) => loadModel({ e, ...functionProps }) },
-        context: {
-            error: {
-                cancelable: true,
-                retryButtonText: 'Nouveau fichier',
-                functions: {
-                    onClick: (e) => loadModel({ e, ...functionProps }),
-                },
-                title: 'Erreur de chargement',
-                message: '⚠️ Aucun modèle sauvegardé trouvé',
-            },
-        },
-    },
-    {
-        id: 'controls-button-7',
-        label: '💾 Sauvegarder les données',
-        className: 'primary',
-        functions: { onClick: (e) => saveData({ e }) },
-    },
-    {
-        id: 'controls-button-8',
-        label: '📂 Charger les données de comparaison par défaut',
-        className: 'primary',
-        functions: { onClick: (e) => loadDefaultDataArray({ e }) },
-    },
-];
-
+/**
+ * Controls component for rendering action buttons.
+ *
+ * @description This uses the `clickableButtons` from `controls.config.ts` array to render buttons with specific actions.
+ * Each button can trigger a function and may open an alert dialog on error.
+ *
+ * @param buttons - **@default=`clickableButtons`** - Optional array of button configurations.
+ */
 export function Controls({ buttons = clickableButtons }) {
-    const { ...context } = useOutletContext();
-    const [isSuccess, setIsSuccess] = useState({});
-    functionProps = { ...functionProps, ...context, setIsSuccess, isSuccess };
+    const context: contextTypes = useOutletContext();
+    const [isSuccess, setIsSuccess] = useState<ControlsStateTypes>({
+        status: false,
+        id: null,
+        error: null,
+    });
+    Object.assign(functionProps, { ...context, setIsSuccess });
     return (
         <section className="controls">
             {buttons.map((button, index) => (
@@ -102,7 +31,7 @@ export function Controls({ buttons = clickableButtons }) {
                     open={!isSuccess.status && isSuccess.id === button.id}
                     onOpenChange={setIsSuccess}
                     context={{
-                        ...button.context?.error,
+                        ...(button.context?.error ?? {}),
                         error: isSuccess.error,
                     }}
                 >
