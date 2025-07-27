5b0f1d2dddca7ee260b8ffc917eac6a8b10c7e9d
Adrien <adrienquijo@gmail.com>
2025-07-27 16:22:21 +0200

    la props context accepte maintenant un typage dynamique de ce que les boutons intègrent - Il n'y aura plus d'erreur si le bouton n'a pas de propriété context : { error : {}} -

--- src/components/Alerts/alertsTypes.ts
+++ src/components/Alerts/alertsTypes.ts
@@ -1,22 +1,15 @@
+import type { ControlsStateTypes } from '@/components/Controls/controlsTypes.ts';
 import type { clickableButtons } from '@/configs/controls.config.ts';
 import type { AlertDialogProps } from '@radix-ui/react-alert-dialog';
 import type { ReactNode } from 'react';
 
+type AllButtons = (typeof clickableButtons)[number];
+
 export interface AlertDialogButtonProps extends AlertDialogProps {
     children: ReactNode;
-    context?: ButtonErrorContext;
-    // [key: string]: (typeof clickableButtons)[number]['context'];
-    // cancelable?: boolean;
-    // retryButtonText?: string;
-    // title?: string;
-    // error?: string | null;
-    // functions?: {
-    //     [key: string]: (typeof clickableButtons)[number]['functions'];
-    // };
+    context?: Partial<
+        Extract<AllButtons, { context?: unknown }>['context']['error']
+    > & {
+        error: ControlsStateTypes['error'];
+    };
 }
-type ButtonErrorContext = Extract<
-    (typeof clickableButtons)[number],
-    { context?: unknown }
->['context'] extends { error: infer E }
-    ? E & { error?: string | null }
-    : { error?: string | null } | { error?: string | null };
