c1ceb016ca6d0a3d7301e59aa53fe96f5938d791
Adrien <adrienquijo@gmail.com>
2025-07-31 01:05:27 +0200

    Ajout d'une fonction de wait -

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+/**
+ * Crer une promesse qui se resoudra
+ * après un délai défini en paramètre
+ * @param duration - La durée de l'attente
+ * @param message - Message à retourner dans la promesse si besoin
+ */
+export function wait(duration: number, message = '') {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(message);
+        }, duration);
+    });
 }
