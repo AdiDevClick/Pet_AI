95c93db4f6ea76b643d438175837ef627530d03e
Adrien <adrienquijo@gmail.com>
2025-07-31 21:24:53 +0200

    Création d'un composant Provider - Il permet de séparer les callback du state - la variable actions sera réactualisée en cas de changement au niveau des modèles et surtout du isInitialized. - Le reste du temps, elle ne devrait pas bouger - Cela permet de régler une issue de render des cartes à chaque clic d'un utilisateur alors que ce n'est pas un comportement souhaité -

---
+++ src/api/context/animalContext/AnimalModelContext.tsx
@@ -0,0 +1,55 @@
+import type { AnimalModelProviderProps } from '@/api/context/animalContext/animalContextTypes.ts';
+import { useAnimalIdentification } from '@/hooks/models/useAnimalIdentification.ts';
+import { createContext, useMemo } from 'react';
+
+export const AnimalActionsContext = createContext(null!);
+export const AnimalStateContext = createContext<{
+    isInitialized: boolean;
+    status: string;
+} | null>(null);
+
+/**
+ * Context provider using the Animal Identification model.
+ *
+ * @description This provides 2 contexts:
+ * - `AnimalActionsContext` for actions callbacks like comparing animals and adding training pairs.
+ * - `AnimalStateContext` for state information like whether the model is initialized and its status.
+ */
+export const AnimalModelProvider = ({ children }: AnimalModelProviderProps) => {
+    const animalModel = useAnimalIdentification();
+    const actions = useMemo(
+        () => ({
+            compareAnimals: animalModel.compareAnimals,
+            addTrainingPair: animalModel.addTrainingPair,
+        }),
+        [animalModel.model]
+    );
+    const state = useMemo(
+        () => ({
+            isInitialized: animalModel.isInitialized,
+            status: animalModel.status,
+        }),
+        [animalModel.isInitialized, animalModel.status]
+    );
+    return (
+        <AnimalActionsContext.Provider value={actions}>
+            <AnimalStateContext.Provider value={state}>
+                {children}
+            </AnimalStateContext.Provider>
+        </AnimalActionsContext.Provider>
+    );
+};
+
+export const withAnimalModelContext = (Component) => (props) => {
+    return (
+        <AnimalActionsContext.Consumer>
+            {(actions) => (
+                <AnimalStateContext.Consumer>
+                    {(state) => (
+                        <Component {...props} actions={actions} state={state} />
+                    )}
+                </AnimalStateContext.Consumer>
+            )}
+        </AnimalActionsContext.Consumer>
+    );
+};
