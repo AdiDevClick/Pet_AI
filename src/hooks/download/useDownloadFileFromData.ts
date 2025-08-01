0886474c5fa2b31f0bca8928edf85a5764d2eb3d
Adrien <adrienquijo@gmail.com>
2025-08-02 01:13:57 +0200

    Création d'un hook permettant de télécharger des datas sous forme de fichier JSON - Il prend en params les datas ainsi que le setter du composant qui l'appelle - Il invoque un lien <a> qu'il déclenche lui-même - Le composant est faire une assertion dynamique de son contenu - S'il ne la fait pas dynamiquement, il faut lui spécifier le type qu'il reçoit : useDownloadFileFromData<MonType>( { data, state, name } ) ou vérifier que la variable puisse bien être null

---
+++ src/hooks/download/useDownloadFileFromData.ts
@@ -0,0 +1,50 @@
+import type { DownloadFileFromDataProps } from '@/hooks/download/useDownloadFileFromDataTypes.ts';
+import { useEffect } from 'react';
+
+/**
+ * Custom hook to download a file from data.
+ *
+ * @description This hook creates a downloadable file
+ * from the provided data and triggers the download.
+ *
+ * @param data - The data to be downloaded as a file.
+ * @param setState - Function to update the state after the download.
+ * @param fileName - Optional name for the downloaded file, defaults to 'download.json'.
+ */
+export function useDownloadFileFromData<T extends Record<string, unknown>>({
+    data,
+    setState,
+    fileName = 'download.json',
+}: DownloadFileFromDataProps<T>) {
+    useEffect(() => {
+        if (!data) {
+            return;
+        }
+
+        const jsonString = JSON.stringify(data, null, 2);
+
+        const blob = new Blob([jsonString], { type: 'application/json' });
+
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+
+        document.body.appendChild(a);
+
+        a.click();
+
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+
+        // Reset the download state after the file is downloaded
+        setState((prev) => ({
+            ...prev,
+            download: { state: false, data: null },
+            id: null,
+        }));
+    }, [data]);
+
+    return { downloadState: data };
+}
