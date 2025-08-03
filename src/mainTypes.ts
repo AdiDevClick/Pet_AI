ca28c92d47bc016e53a088826d2b4f23df0b76c5
Adrien <adrienquijo@gmail.com>
2025-08-03 13:29:37 +0200

    autre

--- src/mainTypes.ts
+++ src/mainTypes.ts
@@ -1,19 +1,19 @@
 export type contextTypes = {
-    accuracy: number;
-    isOnLoad: boolean;
-    setIsOnLoad: (value: boolean) => void;
-    predictionsCount: number;
-    setPredictionsCount: (value: number) => void;
-    trainingCount: number;
-    setTrainingCount: (value: number) => void;
-    setCount: (value: number) => void;
-    count: number;
-    setResetSystem: (value: boolean) => void;
-    resetSystem: boolean;
+   accuracy: number;
+   isOnLoad: boolean;
+   setIsOnLoad: (value: boolean) => void;
+   predictionsCount: number;
+   setPredictionsCount: (value: number) => void;
+   trainingCount: number;
+   setTrainingCount: (value: number) => void;
+   setCount: (value: number) => void;
+   count: number;
+   setResetSystem: (value: boolean) => void;
+   resetSystem: boolean;
 };
 export type CustomError = {
-    cause?: {
-        message?: string;
-        status?: number;
-    };
+   cause?: {
+      message?: string;
+      status?: number;
+   };
 } & Error;
