093e45b1361f0b8f419d437ee9c4fab4ad34de2a
Adrien <adrienquijo@gmail.com>
2025-08-03 13:34:13 +0200

    ControlsFunctionPropsTypes peut maintenant récupérer dynamiquement les contextTypes ainsi que les propriétés du hook useAnimalIdentification - Ajout du type de TrainModelPropsTypes

--- src/components/Controls/controlsTypes.ts
+++ src/components/Controls/controlsTypes.ts
@@ -1,23 +1,31 @@
-import type { contextTypes } from '@/mainTypes.ts';
-import type { Dispatch, MouseEvent, SetStateAction } from 'react';
+import type { useAnimalIdentification } from "@/hooks/models/useAnimalIdentification.ts";
+import type { contextTypes } from "@/mainTypes.ts";
+import type { OnlyMethods } from "@/types/utilsTypes.ts";
+import type { Dispatch, MouseEvent, SetStateAction } from "react";
 
 export type LoadModelTypes = {
-    e: MouseEvent<HTMLButtonElement>;
-    setButtonState: ControlsFunctionPropsTypes['setButtonState'];
+   e: MouseEvent<HTMLButtonElement>;
+   setButtonState: ControlsFunctionPropsTypes["setButtonState"];
 };
 
 export type ControlsClickableButtonTypes<T> = {
-    [key: string]: T;
+   [key: string]: T;
 };
 
-export interface ControlsFunctionPropsTypes extends contextTypes {
-    setButtonState: Dispatch<SetStateAction<ControlsStateTypes>>;
+export interface ControlsFunctionPropsTypes
+   extends contextTypes,
+      OnlyMethods<ReturnType<typeof useAnimalIdentification>, true> {
+   setButtonState: Dispatch<SetStateAction<ControlsStateTypes>>;
 }
 
 export type ControlsStateTypes = {
-    status: boolean;
-    id: string | null;
-    error: string | null;
-    openModal: boolean;
-    download: { state: boolean; data: null };
+   status: boolean;
+   id: string | null;
+   error: string | null;
+   openModal: boolean;
+   download: { state: boolean; data: null };
 };
+
+export type TrainModelPropsTypes = {
+   e: MouseEvent<HTMLButtonElement>;
+} & ControlsFunctionPropsTypes;
