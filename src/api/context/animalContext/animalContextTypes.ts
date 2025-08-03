78dce2a766bcb7a8af9243e11b6adad4f105dd5c
Adrien <adrienquijo@gmail.com>
2025-08-03 13:32:04 +0200

    Code cleanup -

--- src/api/context/animalContext/animalContextTypes.ts
+++ src/api/context/animalContext/animalContextTypes.ts
@@ -1,24 +1,5 @@
-import type { ReactNode } from 'react';
-
-export type AnimalActionsContextType = {
-    compareAnimals: (image1: string, image2: string) => Promise<void>;
-    addTrainingPair: (image1: string, image2: string) => Promise<void>;
-};
-
-export type AnimalStateContextType = {
-    isInitialized: boolean;
-    status: string;
-};
-
-// <{
-//     compareAnimals: (image1: string, image2: string) => Promise<void>;
-//     addTrainingPair: (
-//         image1: string,
-//         image2: string,
-//         sameAnimal: boolean
-//     ) => Promise<void>;
-// } | null>
+import type { ReactNode } from "react";
 
 export type AnimalModelProviderProps = {
-    children: ReactNode;
+   children: ReactNode;
 };
