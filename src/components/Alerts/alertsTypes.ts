d111cee4383698e06232899542cad36b726a9a9a
Adrien <adrienquijo@gmail.com>
2025-08-05 22:00:46 +0200

    Le typage reflète les mises à jours

--- src/components/Alerts/alertsTypes.ts
+++ src/components/Alerts/alertsTypes.ts
@@ -1,15 +1,17 @@
-import type { ControlsStateTypes } from '@/components/Controls/controlsTypes.ts';
-import type { clickableButtons } from '@/configs/controls.config.ts';
-import type { AlertDialogProps } from '@radix-ui/react-alert-dialog';
-import type { ReactNode } from 'react';
+import type { ControlsStateTypes } from "@/components/Controls/controlsTypes.ts";
+import type { clickableButtons } from "@/configs/controls.config.ts";
+import type { AlertDialogProps } from "@radix-ui/react-alert-dialog";
+import type { ReactNode } from "react";
 
 type AllButtons = (typeof clickableButtons)[number];
 
 export interface AlertDialogButtonProps extends AlertDialogProps {
-    children: ReactNode;
-    context?: Partial<
-        Extract<AllButtons, { context?: unknown }>['context']['error']
-    > & {
-        error: ControlsStateTypes['error'];
-    };
+   children: ReactNode;
+   context?: Partial<
+      Extract<AllButtons, { context?: unknown }>["context"]["error"]
+   > & {
+      error: ControlsStateTypes["error"];
+      id?: string;
+      functions: AllButtons extends { functions: infer F } ? F : never;
+   };
 }
