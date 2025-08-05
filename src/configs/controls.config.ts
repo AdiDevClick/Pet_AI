8be94e1f45339f082cbb96c1b769622dfc1d6ce6
Adrien <adrienquijo@gmail.com>
2025-08-05 22:11:09 +0200

    code cleanup

--- src/configs/controls.config.ts
+++ src/configs/controls.config.ts
@@ -1,16 +1,16 @@
 import {
    loadDefaultDataArray,
-   loadModel,
    loadNewImages,
+   openFileExplorer,
    predictAllImages,
    resetSystem,
    saveData,
    saveModel,
    trainModel,
    validateAllImages,
-} from '@/components/Controls/controlsFunctions.ts';
-import type { ControlsFunctionPropsTypes } from '@/components/Controls/controlsTypes.ts';
-import type { MouseEvent } from 'react';
+} from "@/components/Controls/controlsFunctions.ts";
+import type { ControlsFunctionPropsTypes } from "@/components/Controls/controlsTypes.ts";
+import type { MouseEvent } from "react";
 
 /**
  * Config file for controls component.
@@ -28,92 +28,88 @@ export const functionProps = {} as ControlsFunctionPropsTypes;
  */
 export const clickableButtons = [
    {
-      id: 'controls-button-0',
-      label: '🔄 Nouvelles Images',
-      className: 'primary',
+      id: "controls-button-0",
+      label: "🔄 Nouvelles Images",
+      className: "primary",
       functions: {
          onClick: (e: MouseEvent<HTMLButtonElement>) =>
             loadNewImages({ e, ...functionProps }),
       },
    },
    {
-      id: 'controls-button-1',
-      label: '🗑️ Réinitialiser',
-      className: 'danger',
+      id: "controls-button-1",
+      label: "🗑️ Réinitialiser",
+      className: "danger",
       functions: {
          onClick: (e: MouseEvent<HTMLButtonElement>) =>
             resetSystem({ e, ...functionProps }),
       },
    },
    {
-      id: 'controls-button-2',
-      label: '✅ Valider toutes les Images',
-      className: 'success',
+      id: "controls-button-2",
+      label: "✅ Valider toutes les Images",
+      className: "success",
       functions: {
          onClick: (e: MouseEvent<HTMLButtonElement>) =>
             validateAllImages({ e, ...functionProps }),
       },
    },
    {
-      id: 'controls-button-3',
-      label: '🔮 Prédire Tout',
-      className: 'success',
+      id: "controls-button-3",
+      label: "🔮 Prédire Tout",
+      className: "success",
       functions: {
          onClick: (e: MouseEvent<HTMLButtonElement>) =>
             predictAllImages({ e, ...functionProps }),
       },
    },
    {
-      id: 'controls-button-4',
-      label: '🔧 Entraîner le Modèle',
-      className: 'success',
+      id: "controls-button-4",
+      label: "🔧 Entraîner le Modèle",
+      className: "success",
       functions: {
          onClick: (e: MouseEvent<HTMLButtonElement>) =>
             trainModel({ e, ...functionProps }),
       },
    },
    {
-      id: 'controls-button-5',
-      label: '💾 Sauvegarder le modèle',
-      className: 'primary',
+      id: "controls-button-5",
+      label: "💾 Sauvegarder le modèle",
+      className: "primary",
       functions: {
          onClick: (e: MouseEvent<HTMLButtonElement>) =>
             saveModel({ e, ...functionProps }),
       },
    },
    {
-      id: 'controls-button-6',
-      label: '📂 Charger le modèle',
-      className: 'primary',
+      id: "controls-button-6",
+      label: "📂 Charger le modèle",
+      className: "primary",
       functions: {
          onClick: (e: MouseEvent<HTMLButtonElement>) =>
-            loadModel({ e, ...functionProps }),
+            openFileExplorer({ e, ...functionProps }),
       },
       context: {
          error: {
             cancelable: true,
-            retryButtonText: 'Nouveau fichier',
-            functions: {
-               onClick: (e: MouseEvent<HTMLButtonElement>) =>
-                  loadModel({ e, ...functionProps }),
-            },
-            title: 'Erreur de chargement',
-            message: '⚠️ Aucun modèle sauvegardé trouvé',
+            retryButtonText: "Nouveau fichier",
+            title: "Souhaitez-vous charger un nouveau fichier ?",
+            message: "Aucun modèle sauvegardé trouvé",
          },
       },
    },
    {
-      id: 'controls-button-7',
-      label: '💾 Sauvegarder les données',
-      className: 'primary',
+      id: "controls-button-7",
+      label: "💾 Sauvegarder les données",
+      className: "primary",
       functions: {
          onClick: (e: MouseEvent<HTMLButtonElement>) => saveData({ e }),
       },
    },
    {
-      id: 'controls-button-8',
-      label: '📂 Charger les données de comparaison par défaut',
-      className: 'primary',
+      id: "controls-button-8",
+      label: "📂 Charger les données de comparaison par défaut",
+      className: "primary",
       functions: {
          onClick: (e: MouseEvent<HTMLButtonElement>) =>
             loadDefaultDataArray({ e }),
