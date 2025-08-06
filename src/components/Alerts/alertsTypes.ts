85a81d7243aa5d2d5ef72927a262c362de12d038
Adrien <adrienquijo@gmail.com>
2025-08-06 12:06:14 +0200

    Mise à jour du typage pour permettre une intellisense dynamique

--- src/components/Alerts/alertsTypes.ts
+++ src/components/Alerts/alertsTypes.ts
@@ -4,15 +4,17 @@ import type { AlertDialogProps } from "@radix-ui/react-alert-dialog";
 import type { ReactNode } from "react";
 
 type AllButtons = (typeof clickableButtons)[number];
+type ButtonFunctions = AllButtons extends { functions: infer F } ? F : never;
+type ButtonError = AllButtons extends { context?: { error?: infer E } }
+   ? E
+   : unknown;
 
 export interface AlertDialogButtonProps extends AlertDialogProps {
    children: ReactNode;
-   context?: Partial<
-      Extract<AllButtons, { context?: unknown }>["context"]["error"]
-   > & {
-      error?: ControlsStateTypes["error"];
-      id?: string;
-      //   functions?: AllButtons["functions"];
-      functions?: AllButtons extends { functions: infer F } ? F : never;
-   };
+   context?:
+      | Partial<ButtonError> & {
+           error?: ControlsStateTypes["error"];
+           id?: string;
+           functions?: ButtonFunctions;
+        };
 }
