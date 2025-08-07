49b0fcf3d4063b54ca7866851bf30627983c356a
Adrien <adrienquijo@gmail.com>
2025-08-07 23:58:54 +0200

    Il est maintenant possible de retourner les différentes erreurs pour informer l'utilisateur des limites qu'il a pu atteindre - Le state ne se met plus à jour plusieures fois mais une seule fois en fin de logique - Il reste encore à alléger le composant et à le typer

--- src/components/Inputs/ImageInput.tsx
+++ src/components/Inputs/ImageInput.tsx
@@ -1,64 +1,86 @@
-import { ChangeEvent, useRef, useState } from 'react';
-import '@css/image-input.scss';
-import { Button } from '@/components/Buttons/Button.tsx';
+import { useRef, type ChangeEvent } from "react";
+import "@css/image-input.scss";
+import { Button } from "@/components/Buttons/Button.tsx";
 import {
-    ACCEPTED_FILE_MIME_TYPES,
-    FILE_MIME_NOT_ACCEPTED_ERROR,
-    FILE_SIZE_LIMIT_EXCEEDED_ERROR,
-    MAX_FILE_SIZE,
-} from '@/configs/file.config.ts';
+   ACCEPTED_FILE_MIME_TYPES,
+   FILE_MIME_NOT_ACCEPTED_ERROR,
+   FILE_SIZE_LIMIT_EXCEEDED_ERROR,
+   MAX_FILE_SIZE,
+} from "@/configs/file.config.ts";
 
-export function ImageInput({ setInputImages, previewId, ...props }) {
-    const [errors, setErrors] = useState(new Map());
-    const imagePreviewRef = useRef<HTMLDivElement>(null!);
-    const inputRef = useRef<HTMLInputElement>(null!);
-    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const target = event.target;
-        const file = target.files?.[0];
+export function ImageInput({ setPageState, pageState, previewId, ...props }) {
+   const imagePreviewRef = useRef<HTMLDivElement>(null!);
+   const inputRef = useRef<HTMLInputElement>(null!);
 
-        if (file) {
-            if (file.size > MAX_FILE_SIZE) {
-                return setErrors((prev) =>
-                    prev.set(FILE_SIZE_LIMIT_EXCEEDED_ERROR, false)
-                );
-            }
-            if (!ACCEPTED_FILE_MIME_TYPES.includes(file.type)) {
-                return setErrors((prev) =>
-                    prev.set(FILE_MIME_NOT_ACCEPTED_ERROR, false)
-                );
-            }
+   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const target = event.target;
+      const file = target.files?.[0];
+      const errors: string[] = [];
 
-            // Add image preview to the card
-            const image = new Image();
-            image.src = URL.createObjectURL(file);
+      if (file) {
+         if (file.size > MAX_FILE_SIZE) {
+            errors.push(FILE_SIZE_LIMIT_EXCEEDED_ERROR);
+         }
 
-            target.classList.add('filled');
-            imagePreviewRef.current.style.backgroundImage = `url(${image.src})`;
-            setInputImages((prev) => ({ ...prev, [previewId]: image }));
-        } else {
-            target.classList.remove('filled');
-            imagePreviewRef.current.removeAttribute('style');
-        }
-    };
+         if (!ACCEPTED_FILE_MIME_TYPES.includes(file.type)) {
+            errors.push(FILE_MIME_NOT_ACCEPTED_ERROR);
+         }
 
-    return (
-        <div ref={imagePreviewRef} className="image-preview" {...props}>
-            <label
-                className="image-preview__title"
-                htmlFor={`image-upload-${previewId}`}
-            >
-                {props.label}
-            </label>
-            <input
-                ref={inputRef}
-                className="image-preview__input"
-                type="file"
-                accept="image/*"
-                onChange={handleFileChange}
-                id={`image-upload-${previewId}`}
-            />
-            <Button>Sélectionner une image</Button>
-            <p className="image-preview__info">jpg, png : 10mo max</p>
-        </div>
-    );
+         if (errors.length > 0) {
+            target.classList.remove("filled");
+            imagePreviewRef.current.removeAttribute("style");
+            setPageState((prev) => ({
+               ...prev,
+               error: prev.error.set(inputRef.current.id, errors),
+               inputImages: prev.inputImages
+                  .clone()
+                  .delete(inputRef.current.id),
+            }));
+            return;
+         }
+
+         // Add image preview to the card
+         const image = new Image();
+         image.src = URL.createObjectURL(file);
+
+         target.classList.add("filled");
+         imagePreviewRef.current.style.backgroundImage = `url(${image.src})`;
+
+         setPageState((prev) => ({
+            ...prev,
+            error: prev.error.has(inputRef.current.id)
+               ? prev.error.clone().delete(inputRef.current.id)
+               : prev.error,
+            inputImages: prev.inputImages
+               .clone()
+               .set(inputRef.current.id, image),
+         }));
+
+         // Clear previous errors for this input
+      } else {
+         target.classList.remove("filled");
+         imagePreviewRef.current.removeAttribute("style");
+      }
+   };
+
+   return (
+      <div ref={imagePreviewRef} className="image-preview" {...props}>
+         <label
+            className="image-preview__title"
+            htmlFor={`image-upload-${previewId}`}
+         >
+            {props.label}
+         </label>
+         <input
+            ref={inputRef}
+            className="image-preview__input"
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+            id={`image-upload-${previewId}`}
+         />
+         <Button>Sélectionner une image</Button>
+         <p className="image-preview__info">jpg, png : 10mo max</p>
+      </div>
+   );
 }
