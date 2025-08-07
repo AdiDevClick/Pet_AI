c4400253fb99d33c6fbaf1255c11255a3985bfe1
Adrien <adrienquijo@gmail.com>
2025-08-07 23:49:42 +0200

    autre

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,17 +1,17 @@
-import '@css/tasks.scss';
-import { memo } from 'react';
+import "@css/tasks.scss";
+import { memo } from "react";
 export const MemoizedTasks = memo(function Tasks({ children }) {
-    return (
-        <section className="tasks">
-            <div className="task-description">
-                <h3>📋 Tâche actuelle</h3>
-                <p>
-                    Sélectionnez toutes les images qui contiennent des
-                    <strong> {children}</strong>
-                </p>
-            </div>
-        </section>
-    );
+   return (
+      <section className="tasks">
+         <div className="task-description">
+            <h3>📋 Tâche actuelle</h3>
+            <p>
+               Sélectionnez toutes les images qui contiennent des
+               <strong> {children}</strong>
+            </p>
+         </div>
+      </section>
+   );
 });
 
 export default MemoizedTasks;
