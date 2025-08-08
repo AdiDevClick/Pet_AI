039a0d36eefd7eba75d1c24c493cfa4ab73be0ae
Adrien <adrienquijo@gmail.com>
2025-08-08 20:29:53 +0200

    autre

--- src/components/Cards/CardsGrid.tsx
+++ src/components/Cards/CardsGrid.tsx
@@ -1,24 +1,29 @@
-import { TrainingCard } from '@/components/Cards/TrainingCard';
-import { GenericGrid } from '@/components/Grid/GenericGrid.tsx';
+import { TrainingCard } from "@/components/Cards/TrainingCard";
+import { GenericGrid } from "@/components/Grid/GenericGrid.tsx";
 // import '@css/generic-grid.scss';
 // import '@css/card.scss';
 
 export function CardsGrid({
-    images,
-    animalName,
+   images,
+   animalName,
 }: {
-    images: Array<{ id: string; url: string; description: string }>;
-    animalName: string;
+   images: Array<{ id: string; url: string; description: string }>;
+   animalName: string;
 }) {
-    return (
-        <GenericGrid>
-            {images.map((image) => (
-                <TrainingCard
-                    key={image.id}
-                    image={image}
-                    animalName={animalName}
-                />
-            ))}
-        </GenericGrid>
-    );
+   return (
+      <GenericGrid>
+         {images.map((image) => (
+            <TrainingCard
+               key={image.id}
+               image={image}
+               animalName={animalName}
+            />
+         ))}
+         {/* <GenericGrid>
+            <GenericList items={images}>
+               <TrainingCard image={item} animalName={animalName} />
+            </GenericList>
+         </GenericGrid> */}
+      </GenericGrid>
+   );
 }
