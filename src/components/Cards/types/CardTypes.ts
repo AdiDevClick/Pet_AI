49104578381da780a6f868dc4782425e745614b4
Adrien <adrienquijo@gmail.com>
2025-08-08 20:31:08 +0200

    Ajout d'un fichier contenant les types pour les cards

--- src/components/Cards/types/CardTypes.ts
+++ src/components/Cards/types/CardTypes.ts
@@ -1,1 +1,7 @@
-import type { ReactNode } from "react";
+import type { HTMLAttributes, ReactNode } from "react";
+
+export type GenericCardProps<T = unknown> = {
+   children: ReactNode;
+   item?: T;
+   index?: number;
+} & HTMLAttributes<HTMLDivElement>;
