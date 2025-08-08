672bd3b7bc64a9131494f8705a757a87e21f2c10
Adrien <adrienquijo@gmail.com>
2025-08-09 00:35:05 +0200

    Le composant est maintenant plus générique, il pourrait être utilisé dans d'autres cas

--- src/components/Inputs/ImageInput.tsx
+++ src/components/Inputs/ImageInput.tsx
@@ -1,74 +1,82 @@
 import { useRef, type ChangeEvent } from "react";
 import "@css/image-input.scss";
 import { Button } from "@/components/Buttons/Button.tsx";
-import {
-   ACCEPTED_FILE_MIME_TYPES,
-   FILE_MIME_NOT_ACCEPTED_ERROR,
-   FILE_SIZE_LIMIT_EXCEEDED_ERROR,
-   MAX_FILE_SIZE,
-} from "@/configs/file.config.ts";
+import type { ImageInputProps } from "@/components/Inputs/types/InputsTypes.ts";
+import { updateState, verifyFile } from "@/lib/utils.ts";
+import { UniqueSet } from "@/lib/UniqueSet.ts";
 
-export function ImageInput({ setPageState, previewId, ...props }) {
+/**
+ * Input containing an image upload field.
+ *
+ * @description This component allows users to
+ * select an image file.
+ * The image will then be displayed as a preview.
+ *
+ * @param item - `Optional` The item associated with the image input.
+ * @param index - `Optional` The index of the image input.
+ * @param setter - The function to update the page state.
+ * @param state - The current page state.
+ * @param props - This accepts any `HTMLDivElement` props.
+ */
+export function ImageInput<
+   T extends {
+      error: UniqueSet<string, string[]>;
+      inputImages: UniqueSet<string, HTMLImageElement>;
+   }
+>({ item, index, setter, state, ...props }: ImageInputProps<T>) {
    const imagePreviewRef = useRef<HTMLDivElement>(null!);
    const inputRef = useRef<HTMLInputElement>(null!);
+
+   const inputId = `image-upload-${item?.id ?? ""}`;
+   const inputLabel = item?.label ?? "Ajoutez votre image";
 
    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
       const target = event.target;
       const file = target.files?.[0];
-      const errors: string[] = [];
 
       if (file) {
-         if (file.size > MAX_FILE_SIZE) {
-            errors.push(FILE_SIZE_LIMIT_EXCEEDED_ERROR);
-         }
-
-         if (!ACCEPTED_FILE_MIME_TYPES.includes(file.type)) {
-            errors.push(FILE_MIME_NOT_ACCEPTED_ERROR);
-         }
+         const errors = verifyFile(file);
 
          if (errors.length > 0) {
-            target.classList.remove("filled");
-            imagePreviewRef.current.removeAttribute("style");
-            setPageState((prev) => ({
-               ...prev,
-               error: prev.error.set(inputRef.current.id, errors),
-               inputImages: prev.inputImages
-                  .clone()
-                  .delete(inputRef.current.id),
-            }));
+            resetInputVisualAndStyles(target, imagePreviewRef.current);
+
+            updateState(
+               {
+                  // Clear previous errors for this input
+                  // And add new ones
+                  error: (prev) =>
+                     prev
+                        .delete(inputRef.current.id)
+                        .set(inputRef.current.id, errors),
+                  inputImages: (prev) => prev.delete(inputRef.current.id),
+               } as Parameters<typeof updateState<T>>[0],
+               setter
+            );
             return;
          }
-         // Add image preview to the card
          const image = new Image();
          image.src = URL.createObjectURL(file);
 
+         // Add image preview to the card
          target.classList.add("filled");
          imagePreviewRef.current.style.backgroundImage = `url(${image.src})`;
 
-         setPageState((prev) => ({
-            ...prev,
-            // Clear previous errors for this input
-            error: prev.error.has(inputRef.current.id)
-               ? prev.error.clone().delete(inputRef.current.id)
-               : prev.error,
-            inputImages: prev.inputImages
-               .clone()
-               .delete(inputRef.current.id)
-               .set(inputRef.current.id, image),
-         }));
+         updateState(
+            {
+               error: (prev) => prev.delete(inputRef.current.id),
+               inputImages: (prev) => prev.set(inputRef.current.id, image),
+            } as Parameters<typeof updateState<T>>[0],
+            setter
+         );
       } else {
-         target.classList.remove("filled");
-         imagePreviewRef.current.removeAttribute("style");
+         resetInputVisualAndStyles(target, imagePreviewRef.current);
       }
    };
 
    return (
       <div ref={imagePreviewRef} className="image-preview" {...props}>
-         <label
-            className="image-preview__title"
-            htmlFor={`image-upload-${previewId}`}
-         >
-            {props.label}
+         <label className="image-preview__title" htmlFor={inputId}>
+            {inputLabel}
          </label>
          <input
             ref={inputRef}
@@ -76,10 +84,18 @@ export function ImageInput({ setPageState, previewId, ...props }) {
             type="file"
             accept="image/*"
             onChange={handleFileChange}
-            id={`image-upload-${previewId}`}
+            id={inputId}
          />
          <Button>Sélectionner une image</Button>
          <p className="image-preview__info">jpg, png : 10mo max</p>
       </div>
    );
+}
+
+function resetInputVisualAndStyles(
+   input: HTMLInputElement,
+   preview: HTMLDivElement
+) {
+   input.classList.remove("filled");
+   preview.removeAttribute("style");
 }
