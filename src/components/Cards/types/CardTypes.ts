7c3d3dfff684b525cbd30ba1f5177fe0e1566d8a
Adrien <adrienquijo@gmail.com>
2025-08-09 21:53:51 +0200

    Mise à jour des types pour les cards

--- src/components/Cards/types/CardTypes.ts
+++ src/components/Cards/types/CardTypes.ts
@@ -1,3 +1,4 @@
+import type { UniqueSet } from "@/lib/UniqueSet.ts";
 import type { HTMLAttributes, ReactNode } from "react";
 
 export type GenericCardProps<T = unknown> = {
@@ -5,3 +6,24 @@ export type GenericCardProps<T = unknown> = {
    item?: T;
    index?: number;
 } & HTMLAttributes<HTMLDivElement>;
+
+export type PredictionType =
+   | { sameAnimal: boolean; confidence: number }
+   | { error: { status: string | number; message: string; type?: string } };
+export type TrainingTwoCardsState = {
+   results: PredictionType;
+   isCorrect: boolean;
+   showPrediction: boolean;
+   imagesShown: UniqueSet<string, HTMLImageElement>;
+};
+
+export type AnimalItem = {
+   id: string | number;
+   image: string;
+   description: string;
+};
+export type TrainingTwoCardsProps<T extends HTMLAttributes<HTMLDivElement>> = {
+   animals: AnimalItem[];
+   isOnLoad?: boolean;
+   children?: ReactNode;
+} & T;
