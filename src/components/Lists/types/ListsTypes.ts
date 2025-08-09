245d6c037b7a3ce3252337ffd9675b4473ab8003
Adrien <adrienquijo@gmail.com>
2025-08-09 21:51:59 +0200

    mise à jour des types

--- src/components/Lists/types/ListsTypes.ts
+++ src/components/Lists/types/ListsTypes.ts
@@ -2,5 +2,11 @@ import type { ReactElement, ReactNode } from "react";
 
 export type GenericListProps<T> = {
    items: T[];
-   children: ReactElement | ((item: T, index: number) => ReactNode);
+   children:
+      | ReactElement<Partial<{ item: T; index: number }>>
+      | ((item: T, index: number) => ReactNode);
 };
+
+export type GenericList<T> =
+   | ReactElement<Partial<{ item: T; index: number }>>
+   | ReactNode;
