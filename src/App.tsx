5576e419ac7cf7319c47f63fbb98d39198195136
Adrien <adrienquijo@gmail.com>
2025-08-10 13:15:39 +0200

    autre

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { AnimalModelProvider } from '@/api/context/animalContext/AnimalModelContext.tsx';
-import type { ReactNode } from 'react';
+import { AnimalModelProvider } from "@/api/context/animalContext/AnimalModelContext.tsx";
+import type { ReactNode } from "react";
 
 /**
  * @description This component serves as the main layout for the application, wrapping all child components.
@@ -8,9 +8,9 @@ import type { ReactNode } from 'react';
  * @returns
  */
 export function App({ children }: { children: ReactNode }) {
-    return (
-        <AnimalModelProvider>
-            <main className="main-container">{children}</main>
-        </AnimalModelProvider>
-    );
+   return (
+      <AnimalModelProvider>
+         <main className="main-container">{children}</main>
+      </AnimalModelProvider>
+   );
 }
