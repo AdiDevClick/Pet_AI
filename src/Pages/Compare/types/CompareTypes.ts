fe8b82a52d23c9319b18d42e602d4430a0c49eb9
Adrien <adrienquijo@gmail.com>
2025-08-11 00:27:00 +0200

    Ajout de types pour la page compare

--- src/Pages/Compare/types/CompareTypes.ts
+++ src/Pages/Compare/types/CompareTypes.ts
@@ -1,3 +1,14 @@
 import type { initialComparePageState } from "@/Pages/Compare/ComparePets.tsx";
+import type { Dispatch, SetStateAction } from "react";
 
 export type PageState = typeof initialComparePageState;
+
+export type CompareResult = {
+   similarityScore: number;
+   sameAnimal: boolean;
+   confidence: number;
+};
+
+export type ComparePageState = PageState & {
+   setPageState: Dispatch<SetStateAction<PageState>>;
+};
