36faf971c18d4c7d646264bdf545d2c6c162762e
Adrien <adrienquijo@gmail.com>
2025-08-11 11:44:28 +0200

    Ajout d'un lien au bouton vers la page concernée

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,27 +1,30 @@
-import { Button } from '@/components/Buttons/Button.tsx';
-import { GenericTitle } from '@/components/Texts/GenericTitle.tsx';
-import '@css/home.scss';
+import { Button } from "@/components/Buttons/Button.tsx";
+import { GenericTitle } from "@/components/Texts/GenericTitle.tsx";
+import "@css/home.scss";
+import { Link } from "react-router-dom";
 
 export function Home() {
-    return (
-        <div className="home">
-            <h1 className="home__title">Bienvenue sur le Trainee Machine </h1>
-            <GenericTitle>
-                Entraînez un modèle d'IA pour reconnaître des images.
-            </GenericTitle>
-            <p>
-                Rapide et simple à utiliser, vous pourrez créer des modèles d'IA
-                pour vos applications, sites web ou projets personnels.
-                <br />
-                Charger des images et suivez les instructions pour entraîner
-                votre modèle.
-                <br />
-                Vous pourrez ensuite exporter votre modèle pour l'utiliser dans
-                vos projets.
-            </p>
-            <Button className="home__call-to-action-button">
-                <strong>Prêt à commencer ?</strong>
-            </Button>
-        </div>
-    );
+   return (
+      <div className="home">
+         <h1 className="home__title">Bienvenue sur le Trainee Machine </h1>
+         <GenericTitle>
+            Entraînez un modèle d'IA pour reconnaître des images.
+         </GenericTitle>
+         <p>
+            Rapide et simple à utiliser, vous pourrez créer des modèles d'IA
+            pour vos applications, sites web ou projets personnels.
+            <br />
+            Charger des images et suivez les instructions pour entraîner votre
+            modèle.
+            <br />
+            Vous pourrez ensuite exporter votre modèle pour l'utiliser dans vos
+            projets.
+         </p>
+         <Button className="home__call-to-action-button">
+            <Link to="/train-model">
+               <strong>Prêt à commencer ?</strong>
+            </Link>
+         </Button>
+      </div>
+   );
 }
